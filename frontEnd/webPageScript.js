255a1e07de71fd4b3c81754fdf93c72b1b21356a
ShriyanDeyEngineer <your-email@example.com>
2026-04-26 14:51:42 -0500

    progress on the front end, still figuring out how to fetch data from the API/cloud variables and display to the player

---
+++ frontEnd/webPageScript.js
@@ -0,0 +1,14 @@
+// This is a single-line comment
+let x = 5;
+console.log(x);
+
+// HTML span tags have a manipulatable property called "innerHTML"
+
+// If new data is sent to cloud variable that stores the roll numbers, fetch it and show it to the player
+let rollString = "5 2 3 1 8";
+
+// Establish conditionals to change the data held in "rollString"
+            // INSERT CODE HERE //
+
+// Assign the new value to the HTML element that shall display that data to the player
+document.getElementById("current_roll").innerHTML = rollString;
