5f39b28b1e5fc3c2e91b8afd24dd10e6b8682541
ShriyanDeyEngineer <your-email@example.com>
2026-04-26 19:08:50 -0500

    I think this java script code will do the job for fetching and displaying the roll data, just need to make sure that the details of the microcontroller being used match what is used in this code

--- frontEnd/webPageScript.js
+++ frontEnd/webPageScript.js
@@ -1,14 +1,32 @@
-// This is a single-line comment
-let x = 5;
-console.log(x);
+// Define the data associated with the utilized microcontroller and cloud variable
+let identificationDetails = "0a10aced202194944a069dc4";
+let accessToken = "82610000acc77661b40a96934329224927716751";
+let baseURL = "https://api.particle.io/v1/devices/";
+let cloudVariableName_RollData = 'roll';
 
-// HTML span tags have a manipulatable property called "innerHTML"
 
-// If new data is sent to cloud variable that stores the roll numbers, fetch it and show it to the player
-let rollString = "5 2 3 1 8";
+// In the loop function, there will likely be a conditional to decide whether or not to send the current roll data (string data) to the cloud
+// ,so the only thing that needs to be handled here is fetching that data from the cloud and displaying that on the webpage
+//let rollString = "5 2 3 1 8";
 
-// Establish conditionals to change the data held in "rollString"
-            // INSERT CODE HERE //
+
+// Establish an algorithim to fetch the string data held in the cloud variable that represents "rollString"
+
+// This (what to use when working with Particle Device Cloud API) can be found in https://docs.particle.io/reference/cloud-apis/javascript/
+particle.getVariable({ deviceId: identificationDetails, name: cloudVariableName_RollData, auth: accessToken }).then(function(data) {
+  console.log('Device variable retrieved successfully:', data);
+  document.getElementById("current_roll").innerHTML = data;
+}, function(err) {
+  console.log('An error occurred while getting attrs:', err);
+});
+
+
+
+// Alternative, more general method (using the fetch method)
+/*fetch(https://pokeapi.co/api/v2/pokemon/rayquaza)
+    .then(Response => Response.jSon)
+    .catch();*/
+
 
 // Assign the new value to the HTML element that shall display that data to the player
-document.getElementById("current_roll").innerHTML = rollString;
+//document.getElementById("current_roll").innerHTML = rollString; // HTML span tags have a manipulatable property called "innerHTML"
