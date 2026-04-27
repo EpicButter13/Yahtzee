4e8c50edcedb77be6f111d416067115145376941
EpicButter13 <aadi.sharma1325@gmail.com>
2026-04-26 23:29:11 -0500

    edited

--- src/Yahtzee.cpp
+++ src/Yahtzee.cpp
@@ -56,7 +56,7 @@ int diceRoll3 = fiveDice->getDiceValue(3);
 int diceRoll4 = fiveDice->getDiceValue(4);
 int diceRoll5 = fiveDice->getDiceValue(5);
 int rollsLeft = fiveDice->getRolls();
-String score;
+String scoreData;
 LiquidCrystal lcd(
   LCD_RS_PIN,
   LCD_E_PIN,
