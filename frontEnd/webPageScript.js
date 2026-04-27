d991228926e4519fb22a5cfa03c9d11ba55afaf6
Shreyas654 <shreyas.h.pradeep@gmail.com>
2026-04-27 00:19:51 -0500

    webpage done

--- frontEnd/webPageScript.js
+++ frontEnd/webPageScript.js
@@ -1,36 +1,5 @@
-// Define the data associated with the utilized microcontroller and cloud variable
-let identificationDetails = "0a10aced202194944a069dc4";
-let accessToken = "82610000acc77661b40a96934329224927716751";
-let cloudVariableName_RollData = "roll";
+let el = document.getElementById("current_roll");
 
-
-// initialize Particle API
-const particle = new Particle();
-
-
-// FUNCTION to update UI
-function updateRoll() {
-  particle.getVariable({
-    deviceId: identificationDetails,
-    name: cloudVariableName_RollData,
-    auth: accessToken
-  }).then(function(data) {
-
-    console.log("Cloud response:", data);
-
-    let rollString = data.body.result;
-
-    document.getElementById("current_roll").textContent = rollString;
-
-  }).catch(function(err) {
-    console.log("Particle error:", err);
-  });
+if (el) {
+  el.textContent = rollString;
 }
-
-
-// run once immediately
-updateRoll();
-
-
-// then poll every 2 seconds
-setInterval(updateRoll, 2000);
